da06afd3597ba076055a7420feb2d6132755aea7
Ben Cornelis <bencornelis02@gmail.com>
2017-10-28 22:14:26 -0700

    remove related podcasts that were entered in the search

--- app/models/podcast_search.rb
+++ app/models/podcast_search.rb
@@ -6,26 +6,30 @@ class PodcastSearch < ApplicationRecord
     # there will be repetitions across podcasts
     podcasts = scrape_related_podcasts
 
+    # do not include related podcasts already in the search
+    search_ids = itunes_podcast_ids
+    podcasts = podcasts.reject { |pod| search_ids.include?(pod[:itunes_id]) }
+
     # count the appearances of each related podcast
     podcast_to_count = podcasts.map { |pod| [pod, podcasts.count(pod)] }.to_h
 
     # sort podcasts by number of appearances
     podcasts
       .uniq
-      .sort_by { |podcast| -podcast_to_count[podcast] }
+      .sort_by { |pod| -podcast_to_count[pod] }
       .take(5)
   end
 
   def scrape_related_podcasts
     fetch_itunes_podcast_pages.flat_map do |page|
       page.css('.lockup.small.podcast.audio').map do |podcast_node|
+        itunes_id = podcast_node.attributes['adam-id'].value
         title     = podcast_node.attributes['preview-title'].value
-        id        = podcast_node.attributes['adam-id'].value
         image_url = podcast_node.css('div.artwork img')
                       .first.attributes['src-swap-high-dpi'].value
 
         {
-          id:        id, # included so we can remove podcasts that were entered in the search
+          itunes_id: itunes_id, # so we can remove podcasts that were entered in the search
           title:     title,
           image_url: image_url
         }
@@ -49,6 +53,10 @@ class PodcastSearch < ApplicationRecord
 
     hydra.run
     responses.map { |response| Nokogiri::HTML(response.body) }
+  end
+
+  def itunes_podcast_ids
+    podcasts.pluck(:itunes_id)
   end
 
   def itunes_podcast_urls
